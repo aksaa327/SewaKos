da3bf4fefed16216b48b68242aa68132fccd6d34
Aqsa327 <muhammadaksa271@gmail.com>
2024-04-27 15:38:34 +0800

    autentication part 1

--- app/src/main/java/com/example/sewakos/Autentication/LogIn.java
+++ app/src/main/java/com/example/sewakos/Autentication/LogIn.java
@@ -5,19 +5,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.sewakos.PencariKos.Beranda;
 import com.example.sewakos.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LogIn extends AppCompatActivity {
 
-    EditText edtPassword;
+    private TextView daftar;
+
+    private TextInputEditText etEmail, etPassword;
+    private RelativeLayout button_masuk;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +44,45 @@ public class LogIn extends AppCompatActivity {
             return insets;
         });
 
-        edtPassword = findViewById(R.id.edtPassword);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+
+        button_masuk = findViewById(R.id.button_masuk);
+        daftar = findViewById(R.id.daftar);
+
+        auth = FirebaseAuth.getInstance();
+
+        button_masuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            if (auth.getCurrentUser().isEmailVerified()) {
+                                Toast.makeText(getApplicationContext(), "Login Berhasil", Toast.LENGTH_SHORT).show();
+
+                                startActivity(new Intent(getApplicationContext(), Beranda.class));
+                            } else {
+                                etEmail.setError("Verifikasi Email Anda Terlebih Dahulu");
+                            }
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Login Gagal", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        daftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Register.class));
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/sewakos/Autentication/Register.java
+++ app/src/main/java/com/example/sewakos/Autentication/Register.java
@@ -1,17 +1,38 @@
 package com.example.sewakos.Autentication;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.sewakos.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
 
+    private TextView log_in;
+
+    private TextInputEditText etUsername, etEmail, etPassword, etRepassword;
+    private RelativeLayout button_daftar;
+
+    private FirebaseAuth auth;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +42,70 @@ public class Register extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        log_in = findViewById(R.id.log_in);
+
+        etUsername = findViewById(R.id.etUsername);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        etRepassword = findViewById(R.id.etRepassword);
+
+        button_daftar = findViewById(R.id.button_daftar);
+
+        auth = FirebaseAuth.getInstance();
+
+        log_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Register.this, LogIn.class));
+            }
+        });
+
+        button_daftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText().toString();
+                String email = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+                String repassword = etRepassword.getText().toString();
+
+                if (!username.isEmpty() && !email.isEmpty() && !password.isEmpty() && !repassword.isEmpty()) {
+
+                    if (password.length() > 6) {
+                        if (repassword.equals(password)) {
+                            auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        auth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    Toast.makeText(Register.this, "Daftar berhasil", Toast.LENGTH_SHORT).show();
+
+                                                    startActivity(new Intent(Register.this, LogIn.class));
+                                                } else {
+                                                    Toast.makeText(Register.this, "Verifikasi gagal di kirim", Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        });
+                                    } else {
+                                        Toast.makeText(Register.this, "Daftar gagal", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        } else {
+                            etRepassword.setError("Password tidak sama!!!");
+                        }
+                    } else {
+                        etPassword.setError("Password Harus Lebih dari 6 karakter!!!");
+                    }
+
+                } else {
+                    Toast.makeText(Register.this, "Ada data yang masih kosong!!!", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
